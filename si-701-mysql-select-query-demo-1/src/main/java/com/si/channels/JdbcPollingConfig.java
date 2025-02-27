07b05b83029c62abd5a77e4cb0fdcc9c4abebbac
shindetej <shindetejasm@gmail.com>
2025-02-27 10:52:28 +0530

    Spring integration with database | code clean up for polling adaptor

--- si-701-mysql-select-query-demo-1/src/main/java/com/si/channels/JdbcPollingConfig.java
+++ si-701-mysql-select-query-demo-1/src/main/java/com/si/channels/JdbcPollingConfig.java
@@ -7,7 +7,6 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.core.MessageSource;
@@ -28,20 +27,16 @@ public class JdbcPollingConfig {
 		return new DirectChannel();
 	}
 
-	@Bean
-	JdbcTemplate jdbcTemplate(DataSource dataSource) {
-		return new JdbcTemplate(dataSource);
-	}
 
 	@Bean
 	public MessageSource<?> jdbcPollingAdapter(JdbcTemplate jdbcTemplate) {
 		return new JdbcPollingChannelAdapter(jdbcTemplate,
-				"SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, dept_name AS deptName, salary\r\n"
+				"SELECT emp_no as empNo, CONCAT(first_name, ' ', last_name) AS name, dept_name AS deptName, salary\r\n"
 						+ "FROM employees\r\n" + "WHERE salary > 70000 AND dept_name = 'Engineering';\r\n");
 	}
 	
 	@Bean
-	public IntegrationFlow pollingFlow(JdbcTemplate jdbcTemplate) {
+	 IntegrationFlow pollingFlow(JdbcTemplate jdbcTemplate) {
 	    return IntegrationFlow
 	            .from(jdbcPollingAdapter(jdbcTemplate), 
 	                  c -> c.poller(Pollers.fixedDelay(10000)))  // 10 seconds delay
@@ -51,20 +46,12 @@ public class JdbcPollingConfig {
 
 
 	@Bean
-	public PollerMetadata poller() {
-		PollerMetadata poller = new PollerMetadata();
-		poller.setTrigger(new PeriodicTrigger(Duration.ofSeconds(10)));
-		return poller;
-	}
-
-	@SuppressWarnings("unchecked")
-	@Bean
 	@ServiceActivator(inputChannel = "employeeChannel")
-	public MessageHandler consolePrinter() {
+	public MessageHandler printService() {
 		return message -> {
 			for (Map<String, Object> row : (Iterable<Map<String, Object>>) message.getPayload()) {
-				System.out.println("Employee ID: " + row.get("emp_no") + ", Name: " + row.get("name")
-						+ ", Salary: " + row.get("salary"));
+				System.out.println("Employee ID: " + row.get("empNo") + ", Name: " + row.get("name")
+						+ ", Salary: " + row.get("salary")+ ", Department: " + row.get("deptName"));
 			}
 			System.out.println("\n ================== \n");
 		};
