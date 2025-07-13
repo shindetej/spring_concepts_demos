455b3e4039dc18136114bcf781153bf416084c96
shindetej <shindetejasm@gmail.com>
2025-07-13 23:49:28 +0530

    service discovery using eureka server and client

---
+++ sde_product_client/src/main/java/com/si/demo/SdeProductClientApplication.java
@@ -0,0 +1,13 @@
+package com.si.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdeProductClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdeProductClientApplication.class, args);
+	}
+
+}
